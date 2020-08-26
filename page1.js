2e63ec1b95b5eecb34ee1142a7c279bfe6424842
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-08-26 11:45:53 -0700

    added function for changing border-color

---
+++ page1.js
@@ -0,0 +1,36 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  let name = document.getElementById("name");
+  let email = document.getElementById("email");
+  let pass = document.getElementById("pass");
+  // let submitButton = document.getElementById("");
+  
+  name.addEventListener("change", function(){
+    if(name.value !== ""){
+      name.style.borderColor = "#E30246";
+    }else if (name.value === ""){
+      name.style.borderColor = "black";
+    }
+  },false);
+  
+  email.addEventListener("change", function(){
+    if(email.value !== ""){
+      email.style.borderColor = "#E30246";
+    }else if (email.value === ""){
+      email.style.borderColor = "black";
+    }
+  },false);
+
+  pass.addEventListener("change", function(){
+    if(pass.value !== ""){
+      pass.style.borderColor = "#E30246";
+    }else if (pass.value === ""){
+      pass.style.borderColor = "black";
+    }
+  },false);
+
+  // if(name.value !== "" && email.value !== "" && pass.value !== ""){
+    
+  // }
+}, false);
